6e5648e1de9789cd53b7e060ba36ae1dcdd11d8c
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-07 13:54:03 -0600

    update

--- util/fasta2agp.pl
+++ util/fasta2agp.pl
@@ -21,7 +21,7 @@ open (FILE, ">$fasta_outfile") and
 
 
 my $i = 0;# a counter, used for generating unique contig names
-my $x = 1;# counter for numbering column4
+my $x = 0;# counter for numbering column4
 
 my $inseq = Bio::SeqIO->new('-file' => "<$sequence_file",
                '-format' => 'fasta' ) ;
