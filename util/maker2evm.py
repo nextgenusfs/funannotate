9c169faedd9b70983774430f67c10c1046d63276
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-07-08 16:49:12 -0500

    update to maker parsing

---
+++ util/maker2evm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+tr = 'transcript_alignments.gff3'
+pr = 'protein_alignments.gff3'
+gr = 'gene_predictions.gff3'
+
+with open(tr, 'w') as trout:
+    with open(pr, 'w') as prout:
+        with open(gr, 'w') as grout:
+            with open(sys.argv[1], 'rU') as input:
+                for line in input:
+                    if line.startswith('#'):
+                        continue
+                    cols = line.split('\t')
+                    if 'maker' in cols[1]:
+                        if not 'trnascan' in cols[8]:
+                            grout.write(line)
+                    elif 'protein2genome' in cols[1]:
+                        if 'match_part' in cols[2]:
+                            line = line.replace('match_part', 'nucleotide_to_protein_match')
+                            prout.write(line)
+                    elif 'est2genome' in cols[1]:
+                        if 'match_part' in cols[2]:
+                            line = line.replace('match_part', 'EST_match')
+                            trout.write(line)
+                    elif 'cdna2genome' in cols[1]:
+                        if 'match_part' in cols[2]:
+                            line = line.replace('match_part', 'EST_match')
+                            trout.write(line)
