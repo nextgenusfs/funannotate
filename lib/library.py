3d3c2cd67f2c6d120c7692ab0f8aed5ec3235382
Jon Palmer <jon@Jons-MacBook-Pro.local>
2017-04-24 22:53:03 -0500

    trying to fix weird linux GAG bug

--- lib/library.py
+++ lib/library.py
@@ -524,12 +524,8 @@ def gb2output(input, output1, output2, output3):
                                 transcripts.write(">%s\n%s\n" % (f.qualifiers['locus_tag'][0], feature_seq))
 
 def sortGFF(input, output):
-    tmpfile = 'sorting.tmp'
-    with open(tmpfile, 'w') as tmpfile:
-        subprocess.call(['sort', '-k1,1', '-k4,4n', input], stdout = tmpfile)
     with open(output, 'w') as outfile:
-        subprocess.call(['grep', '.', tmpfile], stdout = outfile)
-    os.remove(tmpfile)     
+        subprocess.call(['bedtools', 'sort', '-i', input], stdout = outfile)  
                                 
 def checkGenBank(input):
     count = 0
