3b3a44f6e7949bddc35e8f234e3bdb3311b577a5
Jon Palmer <jon@Jons-MacBook-Pro.local>
2017-10-18 13:45:52 -0400

    update to funannotate clean, should increase speed

--- bin/funannotate-contig_cleaner.py
+++ bin/funannotate-contig_cleaner.py
@@ -2,6 +2,7 @@
 
 import sys, subprocess, os, itertools, argparse
 from Bio import SeqIO
+from Bio.SeqIO.FastaIO import SimpleFastaParser
 
 #setup menu with argparse
 class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
@@ -16,6 +17,8 @@ parser.add_argument('-o','--out', required=True, help='Cleaned output (FASTA)')
 parser.add_argument('-p','--pident', type=int, default=95, help='percent identity of contig')
 parser.add_argument('-c','--cov', type=int, default=95, help='coverage of contig')
 parser.add_argument('-m','--minlen', type=int, default=500, help='Minimum length of contig')
+parser.add_argument('--exhaustive', action='store_true', help='Compute every contig, else stop at N50')
+parser.add_argument('--method', default='mummer', choices=['mummer', 'minimap2'], help='program to use for calculating overlaps')
 args=parser.parse_args()
 
 def which(name):
@@ -37,16 +40,42 @@ def CheckDependencies(input):
         log.error("Missing Dependencies: %s.  Please install missing dependencies and re-run script" % (error))
         sys.exit(1)
 
-def Sortbysize(input):
+def calcN50(input):
+    lengths = []
+    with open(input, 'rU') as infile:
+        for id, sequence in SimpleFastaParser(infile):
+            lengths.append(len(sequence))
+    #now get N50
+    lengths.sort()
+    nlist = []
+    for x in lengths:
+        nlist += [x]*x
+    if len(nlist) % 2 == 0:
+        medianpos = int(len(nlist) / 2)
+        N50 = int((nlist[medianpos] + nlist[medianpos-1]) / 2)
+    else:
+        medianpos = int(len(nlist) / 2)
+        N50 = int(nlist[medianpos])
+    return N50
+
+def Sortbysize(input, n50):
     #sort records and return a list of scaffolds in descending size order
     contigs = []
+    keep = []
     with open(input, 'rU') as input:
         records = list(SeqIO.parse(input, 'fasta'))
         records.sort(cmp=lambda x,y: cmp(len(y),len(x)), reverse=True)
         for rec in records:
-            if len(rec.seq) >= args.minlen:
-                contigs.append(rec.id)
-        return contigs
+            length = len(rec.seq)
+            if length >= args.minlen:
+                if n50:
+                    if length >= n50:
+                        keep.append(rec.id)
+                    else:
+                        contigs.append(rec.id)
+                else:
+                    contigs.append(rec.id)
+        return contigs, keep
 
 def countfasta(input):
     count = 0
@@ -55,24 +84,18 @@ def countfasta(input):
             if line.startswith (">"):
                 count += 1
     return count
-
-def getFasta(sequences, header):
-    with open('query.fa', 'w') as fasta:
-        with open(sequences, 'rU') as input:
-            SeqRecords = SeqIO.parse(input, 'fasta')
-            for rec in SeqRecords:
-                if rec.id == header:
-                    SeqIO.write(rec, fasta, 'fasta')
-
-def getReference(sequences, index, Contigs):
-    #pull out rest of list plus any keepers
+               
+def generateFastas(input, index, Contigs, query):
+    #loop through fasta once, generating query and reference
     contiglist = Contigs[index+1:] + keepers
-    with open('reference.fa', 'w') as output:
-        with open(sequences, 'rU') as input:
-            SeqRecords = SeqIO.parse(input, 'fasta')
-            for rec in SeqRecords:
-                if rec.id in contiglist:
-                    SeqIO.write(rec, output, 'fasta')
+    with open('query.fa', 'w') as qFasta:
+        with open('reference.fa', 'w') as rFasta:
+            with open(input, 'rU') as infile:
+                for Id, Sequence in SimpleFastaParser(infile):
+                    if Id == query:
+                        qFasta.write('>%s\n%s\n' % (Id, Sequence))
+                    elif Id in contiglist:
+                        rFasta.write('>%s\n%s\n' % (Id, Sequence))
 
 def runNucmer(query, reference, output):
     FNULL = open(os.devnull, 'w')
@@ -88,7 +111,7 @@ def runNucmer(query, reference, output):
             cols = line.split('\t')
             match = (float(cols[6]), float(cols[9]))
             if match[0] > args.pident and match[1] > args.cov:
-                print "%s appears duplicated: %i%% identity over %i%% of the contig. contig length: %s " % (output, match[0], match[1], cols[7])
+                print("%s appears duplicated: %i%% identity over %i%% of the contig. contig length: %s " % (output, match[0], match[1], cols[7]))
                 #print match
                 garbage = True
                 break
@@ -96,28 +119,66 @@ def runNucmer(query, reference, output):
             keepers.append(output)
     os.remove(input)
     os.remove(coord_out)
+    
+def runMinimap2(query, reference, output):
+    '''
+    I have not found parameters that mirror mummer yet, do not use minimap method
+    '''
+    FNULL = open(os.devnull, 'w')
+    minitmp = 'minimap.tmp'
+    with open(minitmp, 'w') as out:
+        subprocess.call(['minimap2', '-x', 'asm5', '-N5', reference, query], stdout = out, stderr = FNULL)
+    #now load in results and filter
+    garbage = False #assume this is a good contig
+    with open(minitmp, 'rU') as data:
+        for line in data:
+            line = line.replace('\n', '')
+            qID, qLen, qStart, qEnd, strand, tID, tLen, tStart, tEnd, matches, alnLen, mapQ = line.split('\t')[:12]
+            pident = float(matches) / int(alnLen) * 100
+            coverage = float(alnLen) / int(qLen) * 100
+            print qID, str(qLen), tID, matches, alnLen, str(pident), str(coverage)
+            if pident > args.pident and coverage > args.cov:
+                print("{} appears duplicated: {:.0f}% identity over {:.0f}% of the contig. contig length: {}".format(output, pident, coverage, qLen))
+                garbage = True
+                break
+        if not garbage:
+            keepers.append(output)
+    os.remove(minitmp)
+
 
 #run some checks of dependencies first
 programs = ['nucmer', 'show-coords']
 CheckDependencies(programs)
 
-#now get list of scaffolds, shortest->largest
-scaffolds = Sortbysize(args.input)
+#calculate N50 of assembly
+n50 = calcN50(args.input)
 
-#make a list of contigs that are to keep from the analysis
 global keepers
-keepers = []
+#now get list of scaffolds, shortest->largest
+if args.exhaustive:
+    scaffolds, keepers = Sortbysize(args.input, False)
+else:
+    scaffolds, keepers = Sortbysize(args.input, n50)
 
+print"-----------------------------------------------"
+print("{:,} input contigs, {:,} larger than {:,} bp, N50 is {:,} bp".format(countfasta(args.input), len(scaffolds)+len(keepers), args.minlen, n50))
+if args.exhaustive:
+    print("Checking duplication of {:,} contigs".format(len(scaffolds)))
+else:
+    print("Checking duplication of {:,} contigs shorter than N50".format(len(scaffolds)))
+print"-----------------------------------------------"
 #now loop through the list
 for i in range(0, len(scaffolds)):
-    getFasta(args.input, scaffolds[i])
-    getReference(args.input, i, scaffolds)
-    runNucmer('query.fa', 'reference.fa', scaffolds[i])
+    generateFastas(args.input, i, scaffolds, scaffolds[i])
+    if args.method == 'mummer':
+        runNucmer('query.fa', 'reference.fa', scaffolds[i])
+    elif args.method == 'minimap2':
+        runMinimap2('query.fa', 'reference.fa', scaffolds[i])
     os.remove('query.fa')
     os.remove('reference.fa')
 
-print"------------------------------------"
-print"%i input contigs, %i duplicated, %i written to file" % (countfasta(args.input), (len(scaffolds) - len(keepers)), len(keepers))
+print"-----------------------------------------------"
+print"%i input contigs, %i duplicated, %i written to file" % (countfasta(args.input), (len(keepers) - len(scaffolds)), len(keepers))
 
 #finally write a new reference based on list of keepers
 with open(args.out, 'w') as output:
