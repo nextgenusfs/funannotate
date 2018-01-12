bd1fae7a908a7a628a4cf6f1e71456ffb862bfce
Jon Palmer <jon@Jons-MacBook-Pro.local>
2018-01-11 23:08:41 -0500

    add time

--- util/funannotate-iprscan.py
+++ util/funannotate-iprscan.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, os, argparse, inspect, multiprocessing, urllib2, subprocess
+import sys, os, argparse, inspect, multiprocessing, urllib2, subprocess, time
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
