74ccf460aca88dee10e653e3b4594ec99b50f937
Jon Palmer <jon@Jons-MacBook-Pro.local>
2018-01-11 23:10:55 -0500

    try to fix docker cmd

--- util/funannotate-iprscan.py
+++ util/funannotate-iprscan.py
@@ -141,7 +141,7 @@ def downloadIPRproperties(name, cpus):
     os.remove('ipr.tmp')
     
 def runDocker(input):
-    cmd = ['docker', 'run', '-u', '$UID:$GROUPS', '--rm', '-v', '`pwd`:/dir', '-v', '`pwd`:/in', '-v', '`pwd`/interproscan.properties:/interproscan-5.22-61.0/interproscan.properties', 'blaxterlab/interproscan:latest', 'interproscan.sh', '-i', '/in/'+input, '-d', '/dir', '-dp', '-f', 'XML', '-goterms', '-pa']
+    cmd = ['docker', 'run', '-u', '$UID:$GROUPS', '--rm', '-v', '$PWD:/dir', '-v', '$PWD:/in', '-v', '$PWD/interproscan.properties:/interproscan-5.22-61.0/interproscan.properties', 'blaxterlab/interproscan:latest', 'interproscan.sh', '-i', '/in/'+input, '-d', '/dir', '-dp', '-f', 'XML', '-goterms', '-pa']
     logfile = os.path.join(tmpdir, input.split('.fasta')[0]+'.log')
     with open(logfile, 'w') as log:
         subprocess.call(cmd, cwd=tmpdir, stdout=log, stderr=log)
