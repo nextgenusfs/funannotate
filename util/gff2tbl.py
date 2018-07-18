ce01b774de1cba77f695fe532e47f995caef085b
CFMR NGS <ngs@CFMRs-Mac-Pro.local>
2018-07-18 13:50:37 -0400

    update util gff2tbl with unified tbl writer

--- util/gff2tbl.py
+++ util/gff2tbl.py
@@ -29,153 +29,195 @@ def scaffold2Dict(input):
             if not record.id in scaffLen:
                 scaffLen[record.id] = len(record.seq)
     return scaffLen
-
-def dict2tbl(geneDict, scaffDict):
-    def _sortDict(d):
-        return (d[1]['contig'], d[1]['location'][0])
-    #get ordered dict to get genes in proper location
-    sGenes = sorted(Genes.iteritems(), key=_sortDict)
-    sortedGenes = OrderedDict(sGenes)
-    scaff2genes = {}
-    for k,v in sortedGenes.items():
-        if not v['contig'] in scaff2genes:
-            scaff2genes[v['contig']] = [k]
-        else:
-            scaff2genes[v['contig']].append(k)
-    #now have scaffolds dict and gene dict, loop through scaff dict printing tbl
+                        
+def dicts2tbl(genesDict, scaff2genes, scaffLen, SeqCenter, SeqRefNum):
+    '''
+    function to take funannotate annotation dictionaries and convert to NCBI tbl output
+    '''
+    duplicates = 0
+    pseudo = 0
+    nocds = 0
     for k,v in natsorted(scaff2genes.items()):
         sys.stdout.write('>Feature %s\n' % k)
-        sys.stdout.write('1\t%s\tREFERENCE\n' % scaffDict.get(k))
-        sys.stdout.write('\t\t\t%s\t%s\n' % ('CFMR', '12345'))
+        sys.stdout.write('1\t%s\tREFERENCE\n' % scaffLen.get(k))
+        sys.stdout.write('\t\t\t%s\t%s\n' % (SeqCenter, SeqRefNum))
         for genes in v: #now loop through each gene on the scaffold
-            geneInfo = geneDict.get(genes)
+            geneInfo = genesDict.get(genes) #single funannotate standard dictionary
+            if 'pseudo' in geneInfo:
+                if geneInfo['pseudo']:
+                    log.debug('{:} is pseudo, skipping'.format(genes))
+                    pseudo += 1
+                    continue
+            if geneInfo['type'] == 'mRNA' and not geneInfo['CDS']:
+                log.debug('Skipping {:} because no CDS found.'.format(genes))
+                pseudo += 1
+                continue
+            if geneInfo['type'] == 'mRNA' and not len(geneInfo['ids']) == len(geneInfo['mRNA']) == len(geneInfo['CDS']):
+                log.debug('Incompatible annotation found: {:}\n{:}'.format(genes, geneInfo))
+                duplicates += 1
+                continue
+            if geneInfo['type'] == 'mRNA' and len(geneInfo['CDS']) == 0:
+                nocds += 1
+                continue
+            if geneInfo['type'] == None:
+                continue
             #check for partial models
             if True in geneInfo['partialStart']:
-                partialStart = '<'
+                ps = '<'
             else:
-                partialStart = ''
+                ps = ''
             if True in geneInfo['partialStop']:
-                partialStop = '>'
+                pss = '>'
             else:
-                partialStop = ''
+                pss = ''
+            #if geneInfo['type'] == 'rRNA' or geneInfo['type'] == 'tRNA':
+                #ps = '<'
+                #pss = '>'              
             #now write gene model
             if geneInfo['strand'] == '+':
-                sys.stdout.write('%s%i\t%s%i\tgene\n' % (partialStart, geneInfo['location'][0], partialStop, geneInfo['location'][1]))
+                sys.stdout.write('%s%i\t%s%i\tgene\n' % (ps, geneInfo['location'][0], pss, geneInfo['location'][1]))
+                sys.stdout.write('\t\t\tlocus_tag\t%s\n' % genes)
             else:
-                sys.stdout.write('%s%i\t%s%i\tgene\n' % (partialStart, geneInfo['location'][1], partialStop, geneInfo['location'][0]))
-            if geneInfo['name']:
-            	sys.stdout.write('\t\t\tgene\t%s\n' % geneInfo['name'])
-            sys.stdout.write('\t\t\tlocus_tag\t%s\n' % genes)                                 
-            #support multiple transcripts
-            for i in range(0,len(geneInfo['ids'])):
+                sys.stdout.write('%s%i\t%s%i\tgene\n' % (ps, geneInfo['location'][1], pss, geneInfo['location'][0]))
+                sys.stdout.write('\t\t\tlocus_tag\t%s\n' % genes)                                 
+            #now will output the gene models with -T1, -T2, -T3 annotations based on expression values
+            #means need to get the order
+            order = []
+            if len(geneInfo['ids']) > 1: #multiple transcripts, so get order of highest TPM
+                tpms = []
+                for num,tpm in enumerate(geneInfo['note']):
+                    for item in tpm:
+                        if item.startswith('TPM:'):
+                            value = float(item.split(':')[-1])
+                            tpms.append((value,num))
+                if len(tpms) > 0:
+                    for x in sorted(tpms, reverse=True):
+                        order.append(x[1])
+                else:
+                    order = range(0,len(geneInfo['ids']))
+            else:
+                order.append(0)
+            for num,i in enumerate(order): #now write mRNA and CDS features
+                if geneInfo['ids'][i].startswith('evm.model'): #if from predict, rename to match locus_tag
+                    protein_id = genes+'-T'+str(num+1)
+                else:
+                    protein_id = geneInfo['ids'][i]
                 if geneInfo['type'] == 'mRNA':
                     if geneInfo['partialStart'][i] == False:
-                        partialStart = ''
+                        ps = ''
                     else:
-                        partialStart = '<'
+                        ps = '<'
                     if geneInfo['partialStop'][i] == False:
-                        partialStop = ''
+                        pss = ''
                     else:
-                        partialStop = '>'
+                        pss = '>'
                     if geneInfo['strand'] == '+':
                         for num, exon in enumerate(geneInfo['mRNA'][i]):
                             if num == 0 and num == len(geneInfo['mRNA'][i]) - 1: #single exon, so slightly differnt method
-                                sys.stdout.write('%s%s\t%s%s\tmRNA\n' % (partialStart, exon[0], partialStop, exon[1]))
+                                sys.stdout.write('%s%s\t%s%s\tmRNA\n' % (ps, exon[0], pss, exon[1]))
                             elif num == 0:
-                                sys.stdout.write('%s%s\t%s\tmRNA\n' % (partialStart, exon[0], exon[1]))
+                                sys.stdout.write('%s%s\t%s\tmRNA\n' % (ps, exon[0], exon[1]))
                             elif num == len(geneInfo['mRNA'][i]) - 1: #this is last one
-                                sys.stdout.write('%s\t%s%s\n' % (exon[0], partialStop, exon[1]))
+                                sys.stdout.write('%s\t%s%s\n' % (exon[0], pss, exon[1]))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (exon[0], exon[1]))
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
-                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s-T%i_mrna\n' % (genes,i+1))
-                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s-T%i\n' % (genes, i+1)) 
+                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s_mrna\n' % (protein_id))
+                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s\n' % (protein_id))
                         for num, cds in enumerate(geneInfo['CDS'][i]):
                             if num == 0 and num == len(geneInfo['CDS'][i]) - 1: #single exon, so slightly differnt method
-                                sys.stdout.write('%s%s\t%s%s\tCDS\n' % (partialStart, cds[0], partialStop, cds[1]))
+                                sys.stdout.write('%s%s\t%s%s\tCDS\n' % (ps, cds[0], pss, cds[1]))
                             elif num == 0:
-                                sys.stdout.write('%s%s\t%s\tCDS\n' % (partialStart, cds[0], cds[1]))
+                                sys.stdout.write('%s%s\t%s\tCDS\n' % (ps, cds[0], cds[1]))
                             elif num == len(geneInfo['CDS'][i]) - 1: #this is last one
-                                sys.stdout.write('%s\t%s%s\n' % (cds[0], partialStop, cds[1]))
+                                sys.stdout.write('%s\t%s%s\n' % (cds[0], pss, cds[1]))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (cds[0], cds[1]))
                         sys.stdout.write('\t\t\tcodon_start\t%i\n' % geneInfo['codon_start'][i])
-                        if geneInfo['db_xref'][i]:
-                        	for x in geneInfo['db_xref'][i]:
-                        		sys.stdout.write('\t\t\tdb_xref\t%s\n' % x)
-                        if geneInfo['note'][i]:
-                        	for x in geneInfo['note'][i]:
-                        		sys.stdout.write('\t\t\tnote\t%s\n' % x)
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
-                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s-T%i_mrna\n' % (genes,i+1))
-                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s-T%i\n' % (genes,i+1))                                       
-                    else:
+                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s_mrna\n' % (protein_id))
+                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s\n' % (protein_id))                                
+                    else: #means this is on crick strand            
                         for num, exon in enumerate(geneInfo['mRNA'][i]):
                             if num == 0 and num == len(geneInfo['mRNA'][i]) - 1: #single exon, so slightly differnt method
-                                sys.stdout.write('%s%s\t%s%s\tmRNA\n' % (partialStart, exon[1], partialStop, exon[0]))
+                                sys.stdout.write('%s%s\t%s%s\tmRNA\n' % (ps, exon[1], pss, exon[0]))
                             elif num == 0:
-                                sys.stdout.write('%s%s\t%s\tmRNA\n' % (partialStart, exon[1], exon[0]))
-                            elif num == len(geneInfo['mRNA']) - 1: #this is last one
-                                sys.stdout.write('%s\t%s%s\n' % (exon[1], partialStop, exon[0]))
+                                sys.stdout.write('%s%s\t%s\tmRNA\n' % (ps, exon[1], exon[0]))
+                            elif num == len(geneInfo['mRNA'][i]) - 1: #this is last one
+                                sys.stdout.write('%s\t%s%s\n' % (exon[1], pss, exon[0]))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (exon[1], exon[0]))                 
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
-                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s-T%i_mrna\n' % (genes,i+1))
-                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s-T%i\n' % (genes,i+1))
+                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s_mrna\n' % (protein_id))
+                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s\n' % (protein_id))
                         for num, cds in enumerate(geneInfo['CDS'][i]):
                             if num == 0 and num == len(geneInfo['CDS'][i]) - 1: #single exon, so slightly differnt method
-                                sys.stdout.write('%s%s\t%s%s\tCDS\n' % (partialStart, cds[1], partialStop, cds[0]))
+                                sys.stdout.write('%s%s\t%s%s\tCDS\n' % (ps, cds[1], pss, cds[0]))
                             elif num == 0:
-                                sys.stdout.write('%s%s\t%s\tCDS\n' % (partialStart, cds[1], cds[0]))
-                            elif num == len(geneInfo['CDS'][i]) - 1: #this is last one
-                                sys.stdout.write('%s\t%s%s\n' % (cds[1], partialStop, cds[0]))
+                                sys.stdout.write('%s%s\t%s\tCDS\n' % (ps, cds[1], cds[0]))
+                            elif num == (len(geneInfo['CDS'][i]) - 1): #this is last one
+                                sys.stdout.write('%s\t%s%s\n' % (cds[1], pss, cds[0]))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (cds[1], cds[0]))
                         sys.stdout.write('\t\t\tcodon_start\t%i\n' % geneInfo['codon_start'][i])
-                        if geneInfo['db_xref'][i]:
-                        	for x in geneInfo['db_xref'][i]:
-                        		sys.stdout.write('\t\t\tdb_xref\t%s\n' % x)
-                        if geneInfo['note'][i]:
-                        	for x in geneInfo['note'][i]:
-                        		sys.stdout.write('\t\t\tnote\t%s\n' % x)
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
-                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s-T%i_mrna\n' % (genes,i+1))
-                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s-T%i\n' % (genes,i+1))  
+                        sys.stdout.write('\t\t\ttranscript_id\tgnl|ncbi|%s_mrna\n' % (protein_id))
+                        sys.stdout.write('\t\t\tprotein_id\tgnl|ncbi|%s\n' % (protein_id))
                 elif geneInfo['type'] == 'tRNA':
                     if geneInfo['strand'] == '+':
                         for num, exon in enumerate(geneInfo['mRNA'][i]):
                             if num == 0:
-                                sys.stdout.write('<%s\t>%s\ttRNA\n' % (exon[0], exon[1]))
+                                #sys.stdout.write('<%s\t>%s\t%s\n' % (exon[0], exon[1], geneInfo['type']))
+                                sys.stdout.write('%s\t%s\t%s\n' % (exon[0], exon[1], geneInfo['type']))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (exon[0], exon[1]))
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
                         if geneInfo['product'] == 'tRNA-Xxx':
-                            sys.stdout.write('\t\t\tpseudo\n')
-                        if geneInfo['note'] != '':
-                            sys.stdout.write('\t\t\tnote\t%s\n' % geneInfo['note'][i])                                    
+                            sys.stdout.write('\t\t\tpseudo\n')        
                     else:
-                        sys.stdout.write('<%i\t>%i\tgene\n' % (geneInfo['end'], geneInfo['start']))
-                        sys.stdout.write('\t\t\tlocus_tag\t%s\n' % genes)
                         for num, exon in enumerate(geneInfo['mRNA'][i]):
                             if num == 0:
-                                sys.stdout.write('<%s\t>%s\ttRNA\n' % (exon[1], exon[0]))
+                                #sys.stdout.write('<%s\t>%s\t%s\n' % (exon[1], exon[0], geneInfo['type']))
+                                sys.stdout.write('%s\t%s\t%s\n' % (exon[1], exon[0], geneInfo['type']))
                             else:
                                 sys.stdout.write('%s\t%s\n' % (exon[1], exon[0]))
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
                         if geneInfo['product'] == 'tRNA-Xxx':
                             sys.stdout.write('\t\t\tpseudo\n')
-                        if geneInfo['note'] != '':
-                            sys.stdout.write('\t\t\tnote\t%s\n' % geneInfo['note'][i])
                 elif geneInfo['type'] == 'rRNA':
                     if geneInfo['strand'] == '+':
-                        sys.stdout.write('<%s\t>%s\t%s\n' % (geneInfo['location'][0],geneInfo['location'][1], geneInfo['type']))
+                        #sys.stdout.write('<%s\t>%s\t%s\n' % (geneInfo['location'][0],geneInfo['location'][1], geneInfo['type']))
+                        sys.stdout.write('%s\t%s\t%s\n' % (geneInfo['location'][0],geneInfo['location'][1], geneInfo['type']))
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])   
                     else:
-                        sys.stdout.write('<%s\t>%s\t%s\n' % (geneInfo['location'][1],geneInfo['location'][0], geneInfo['type']))
+                        #sys.stdout.write('<%s\t>%s\t%s\n' % (geneInfo['location'][1],geneInfo['location'][0], geneInfo['type']))
+                        sys.stdout.write('%s\t%s\t%s\n' % (geneInfo['location'][1],geneInfo['location'][0], geneInfo['type']))
                         sys.stdout.write('\t\t\tproduct\t%s\n' % geneInfo['product'][i])
+    if any(i > 0 for i in [duplicates,pseudo,nocds]):
+        sys.stderr.write('Skipped {:,} annotations: {:,} pseudo genes; {:,} no CDS; {:,} duplicated features'.format(sum([pseudo,nocds,duplicates]),pseudo,nocds,duplicates))
+
 
 #load into dictionary
 Genes = {}
 Genes = lib.gff2dict(args.gff3, args.fasta, Genes)
+
+#sort the dictionary
+def _sortDict(d):
+    return (d[1]['location'][0], d[1]['location'][1])
+
+#now sort dictionary by contig and location, rename using prefix, translate to protein space to get proper start/stop info
+sGenes = sorted(Genes.iteritems(), key=_sortDict)
+sortedGenes = OrderedDict(sGenes)
+scaff2genes = {}
+for k,v in sortedGenes.items():
+    if not v['contig'] in scaff2genes:
+        scaff2genes[v['contig']] = [k]
+    else:
+        scaff2genes[v['contig']].append(k)
+
+#get length of scaffolds
 scaffLen = scaffold2Dict(args.fasta)
-dict2tbl(Genes, scaffLen)
+
+#now write table
+dicts2tbl(sortedGenes, scaff2genes, scaffLen, 'CFMR', '12345')
+#dict2tbl(Genes, scaffLen)
