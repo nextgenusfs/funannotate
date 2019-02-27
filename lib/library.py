3129958475aa48a5a0d23b4af1cdcd85facbb388
Package Admin <root@biocluster>
2019-02-27 13:39:24 -0800

    fix typo for genemark ET run - the object is string not array, have to slice it differently to update to default 500

--- lib/library.py
+++ lib/library.py
@@ -4180,8 +4180,9 @@ def RunGeneMarkET(command, input, ini, evidence, maxintron, softmask, cpus, tmpd
         with open(evidence, 'rU') as evid:
             for line in evid:
                 if '\tintron\t' in line and '\tb2h\t' in line:
-                    line[5] = 500 # for intron hint score to be 500
-                    hints.write(line)
+                    tmprow = line.split("\t")
+                    tmprow[5] = "500" # for intron hint score to be 500
+                    hints.write("\t".join(tmprow))
     log.info("Running GeneMark-ET on assembly")
     cmd = [command, '--ET', os.path.abspath(hintsfile), '--max_intron', str(maxintron), '--soft_mask', str(softmask), '--cores', str(cpus), '--sequence', contigs]
     if fungus == 'fungus':
