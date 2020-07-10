c06ff65c3981138ec0353f1eb5e013fa9cf182f0
Jon Palmer <nextgenusfs@gmail.com>
2020-07-10 07:46:08 -0700

    hopfully fixed, forgot a len()

--- funannotate/annotate.py
+++ funannotate/annotate.py
@@ -783,9 +783,8 @@ def main(args):
     with open(os.path.join(outputdir, 'annotate_misc', 'annotations.genes-products.txt'), 'w') as gene_annotations:
         for key, value in natsorted(list(GeneSeen.items())):
             if len(value) > 1:
-                print(key, value)
                 try:
-                    testMultiple = set([x[0].split('-T')[0] for x in value])
+                    testMultiple = len(set([x[0].split('-T')[0] for x in value]))
                 except:
                     testMultiple = len(value)
                 for i in range(0, len(value)):
