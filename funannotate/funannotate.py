a6358872521295c8bd877f50271b6f8107eaf282
Jason Stajich <jasonstajich.phd@gmail.com>
2026-05-18 12:40:49 -0700

    missing import

--- funannotate/funannotate.py
+++ funannotate/funannotate.py
@@ -5,6 +5,7 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import sys
 import os
+import importlib
 import subprocess
 import funannotate
 from funannotate.__version__ import __version__
